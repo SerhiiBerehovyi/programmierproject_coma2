9c259f083679b3d2f2e73798098e375c21298df8
Serhii Berehovyi <berehovyis@gmail.com>
2021-07-05 13:49:37 +0200

    something like sphere

--- main.py
+++ main.py
@@ -11,17 +11,21 @@ def main():
     with Image.open("space.png") as im:
         image = np.array(im)
         image = image.tolist()
-        print(image)
         b, h = im.size
+        
+    im = []
+    for i in range(len(image)):
+        for j in range(len(image)):
+            im.append(image[i][j])
+            
+    Main.include("snapshot_sphere.jl")
+    new_image = Main.snapshot_sphere(b,h,im,(0,0,300),50, 1)
     
-    Main.include("snapshot_sphere.jl")   
-    #jl = Julia()
-    #jl.eval("snapshot_sphere.jl")
-    #new_image = Main.snapshot_sphere(b,h,image,(0,0,300),20, 1)
+    ni = np.array(new_image)
+    im = Image.fromarray(np.uint8(ni)).convert("RGB")
+    im.show()
     
-    #ni = np.array(new_image)
-    #im = Image.fromarray(ni, "RGB")
-    #im.show()
+    
     
 if __name__ == "__main__":
     main()
--- snapshot_sphere.jl
+++ snapshot_sphere.jl
@@ -4,9 +4,8 @@ include("is_visible.jl")
 
 function snapshot_sphere(b,h,daten,m,r,dichte)
     pic = initial_pic()
-    return pic
-    println(typeof(daten))
-    points = []
+    
+    
     for i=1:h
         for j=1:b
             p = samples(j,i, b,h, m, r, dichte)
@@ -15,8 +14,7 @@ function snapshot_sphere(b,h,daten,m,r,dichte)
                 x = camera[1] + 251
                 y = camera[2] + 251
                 
-                d = daten[i*j]
-                pic[x][y] = d
+                pic[x][y] = [daten[i*j], daten[i*j+1], daten[i*j+2]]
             end
         end
     end
@@ -28,7 +26,7 @@ function initial_pic()
     for i=1:500
         row = []
         for j=1:500
-            push!(row, [255, 255, 255])
+            push!(row, [255,255,255])
         end
         push!(pic, row)
     end
