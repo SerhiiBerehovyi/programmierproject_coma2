46fe03d4658e4a7584350d53412083e96b6c1718
Serhii Berehovyi <berehovyis@gmail.com>
2021-07-05 12:29:01 +0200

    crazy bild

--- main.py
+++ main.py
@@ -10,14 +10,18 @@ def main():
     h = 0
     with Image.open("space.png") as im:
         image = np.array(im)
+        image = image.tolist()
+        print(image)
         b, h = im.size
     
     Main.include("snapshot_sphere.jl")   
     #jl = Julia()
     #jl.eval("snapshot_sphere.jl")
-    new_image = Main.snapshot_sphere(b,h,image,(0,0,300),20, 1)
-    im = Image.fromarray(new_image, "RGB")
-    im.show()
+    #new_image = Main.snapshot_sphere(b,h,image,(0,0,300),20, 1)
+    
+    #ni = np.array(new_image)
+    #im = Image.fromarray(ni, "RGB")
+    #im.show()
     
 if __name__ == "__main__":
     main()
--- snapshot_sphere.jl
+++ snapshot_sphere.jl
@@ -4,19 +4,23 @@ include("is_visible.jl")
 
 function snapshot_sphere(b,h,daten,m,r,dichte)
     pic = initial_pic()
-    
+    return pic
+    println(typeof(daten))
     points = []
     for i=1:h
         for j=1:b
             p = samples(j,i, b,h, m, r, dichte)
             if is_visible(p, m ,r)
                 camera = abbild(p)
-                x = camera[1] + 249
-                y = camera[2] + 249
-                pic[x][y] = daten[i][j]
+                x = camera[1] + 251
+                y = camera[2] + 251
+                
+                d = daten[i*j]
+                pic[x][y] = d
             end
         end
-    end    
+    end
+    return pic
 end
 
 function initial_pic()
@@ -24,7 +28,7 @@ function initial_pic()
     for i=1:500
         row = []
         for j=1:500
-            push!(row, [0,0,0])
+            push!(row, [255, 255, 255])
         end
         push!(pic, row)
     end
